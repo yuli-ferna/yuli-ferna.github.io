ba4a916e60271d2625197ae8a7af732cfee88b96
yuli-ferna <yuliferna123@gmail.com>
2020-04-12 16:12:20 -0400

    point lights

--- src/index.js
+++ src/index.js
@@ -35,10 +35,13 @@ function main() {
 		alert('Bang!');
 		},
 
+		//spotlight
 		posX: -0.6, 
 		posY: 20, 
 		posZ: 42,
 		colorL: "#ffffff", // RGB array
+		penunmbra: 0.2,
+		intensity:1,
 		
 		color0: "#000000", // CSS string
 		colorg: "#23ae23", // CSS string
@@ -52,7 +55,6 @@ function main() {
 	// gui.add(obj, 'displayOutline');
 	// gui.add(obj, 'explode');
 
-	// gui.add(obj, 'maxSize').min(-10).max(10).step(0.25);
 	// gui.add(obj, 'height').step(5); // Increment amount
 
 	// // Choose from accepted values
@@ -61,17 +63,7 @@ function main() {
 	// // Choose from named values
 	// gui.add(obj, 'speed', { Stopped: 0, Slow: 0.1, Fast: 5 } );
 	
-	var f1 = gui.addFolder('SpotLight');
-	f1.add(obj, 'posX');
-	f1.add(obj, 'posY');
-	f1.add(obj, 'posZ');
-	f1.addColor(obj, 'colorL');
-
-	var f2 = gui.addFolder('AmbientLight');
-	f2.addColor(obj, 'color0');
-
-	var f3 = gui.addFolder('HemisphereLight');
-	f3.addColor(obj, 'colorg');
+	
 	// f1.addColor(obj, 'color1');
 	// f1.addColor(obj, 'color2');
 	// f1.addColor(obj, 'color3');
@@ -99,11 +91,19 @@ function main() {
 	camera.position.y = 40;
 	camera.position.z = 40;
 	camera.lookAt(scene.position);
+
+
+	// var controls = new OrbitControls( camera, renderer.domElement );
+	// controls.addEventListener( 'change', render );
+	// controls.minDistance = 20;
+	// controls.maxDistance = 500;
+	// controls.enablePan = false;
+
 	//SpotLight
 	var spotLight = new THREE.SpotLight( 0xffff00 );
 	spotLight.castShadow = true;            // default false
 
-	spotLight.position.set( 1, 1, 20 );
+	spotLight.position.set( obj.posX, obj.posY, obj.posZ );
 
 	spotLight.shadow.mapSize.width = 1024;
 	spotLight.shadow.mapSize.height = 1024;
@@ -117,12 +117,29 @@ function main() {
 	//scene.add( spotLightHelper );
 
 	//Ambient light
-	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	var light = new THREE.AmbientLight( obj.color0 ); // soft white light
 	scene.add( light );
 
 	//Hemisphere light
-	var hemisLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+	var hemisLight = new THREE.HemisphereLight( obj.color0, obj.colorg, 1 );
 	scene.add( hemisLight );
+	var light1, light2, light3, light4;
+	//Point lights
+	light1 = new THREE.PointLight( 0xff0040, 2, 50 );
+	light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
+	scene.add( light1 );
+
+	light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
+	light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
+	scene.add( light2 );
+
+	light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
+	light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
+	scene.add( light3 );
+
+	light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
+	light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
+	scene.add( light4 );
 
 	//Cube
 	const boxWidth = 5;
@@ -174,20 +191,75 @@ function main() {
 	var stats = new Stats();
 	stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 	document.body.appendChild( stats.dom );
+
+
+	var f1 = gui.addFolder('SpotLight');
+	f1.add(obj, 'posX').onChange(function (val) {
+		spotLight.position.x = val;
+	});
+	f1.add(obj, 'posY').onChange(function (val) {
+		spotLight.position.y = val;
+	});
+	f1.add(obj, 'posZ').onChange(function (val) {
+		spotLight.position.z = val;
+	});
+	f1.addColor(obj, 'colorL').onChange(function (val) {
+		spotLight.color.set(val);
+	});
+	f1.add(obj, 'penunmbra').min(0).max(1).step(0.1).onChange(function (val) {
+		spotLight.penunmbra = val;
+	});
+	f1.add(obj, 'intensity').min(0).max(1).step(0.1).onChange(function (val) {
+		spotLight.intensity = val;
+	});
+
+	var f2 = gui.addFolder('AmbientLight');
+	f2.addColor(obj, 'color0').onChange(function (val) {
+		light.color.set(val);
+		hemisLight.color.set(val);
+	});
+
+	var f3 = gui.addFolder('HemisphereLight');
+	f3.addColor(obj, 'colorg').onChange(function (val) {
+		hemisLight.groundColor.set(val);
+	});
+	// controls.target.copy( plane.position );
+	// controls.update();
+	function displayWindowSize(){
+        // Get width and height of the window excluding scrollbars
+        var w = document.documentElement.clientWidth;
+        var h = document.documentElement.clientHeight;
+        
+		// Display result inside a div element
+		console.log("Width: " + w + ", " + "Height: " + h);
+		renderer.setSize(w, h);
+		// camera.fov = Math.atan(window.innerHeight / 2 / camera.position.z) * 2 * THREE.Math.RAD2DEG;
+		camera.aspect = w / h;
+		camera.updateProjectionMatrix();
+    }
+    
+    // Attaching the event listener function to window's resize event
+	window.addEventListener("resize", displayWindowSize);
 	
 	function render(time) {
 		stats.begin();
 			time *= 0.001;  // convert time to seconds
-			spotLight.position.x = obj.posX;
-			spotLight.position.y = obj.posY;
-			spotLight.position.z = obj.posZ;
-			
-			spotLight.color.set(obj.colorL);
-			light.color.set(obj.color0);
-			hemisLight.color.set(obj.color0);
-			hemisLight.groundColor.set(obj.colorg);
-			// cube.rotation.x = time;
-			// cube.rotation.y = time;
+			light1.position.x = Math.sin( time * 0.7 ) * 30;
+			light1.position.y = Math.cos( time * 0.5 ) * 40;
+			light1.position.z = Math.cos( time * 0.3 ) * 30;
+
+			light2.position.x = Math.cos( time * 0.3 ) * 30;
+			light2.position.y = Math.sin( time * 0.5 ) * 40;
+			light2.position.z = Math.sin( time * 0.7 ) * 30;
+
+			light3.position.x = Math.sin( time * 0.7 ) * 30;
+			light3.position.y = Math.cos( time * 0.3 ) * 40;
+			light3.position.z = Math.sin( time * 0.5 ) * 30;
+
+			light4.position.x = Math.sin( time * 0.3 ) * 30;
+			light4.position.y = Math.cos( time * 0.7 ) * 40;
+			light4.position.z = Math.sin( time * 0.5 ) * 30;
+
 
 			renderer.render(scene, camera);
 		stats.end();
