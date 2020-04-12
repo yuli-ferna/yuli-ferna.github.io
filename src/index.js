395afc516422846feb2be5d710f5aeb8727adf69
yuli-ferna <yuliferna123@gmail.com>
2020-04-11 21:10:29 -0400

    stats js para evaluar performance

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@
 const THREE = require('three');
 // CommonJS:
 const dat = require('dat.gui');
+const Stats = require('stats.js');
 
 //controles orbitales
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -34,33 +35,31 @@ function main() {
 		alert('Bang!');
 		},
 
-		posX: 1, 
-		posY: 1, 
-		posZ: 20,
+		posX: -0.6, 
+		posY: 20, 
+		posZ: 42,
 		colorL: "#ffffff", // RGB array
 		
-		color0: "#ffae23", // CSS string
-		color1: [ 0, 128, 255 ], // RGB array
-		color2: [ 0, 128, 255, 0.3 ], // RGB with alpha
-		color3: { h: 350, s: 0.9, v: 0.3 } // Hue, saturation, value
+		color0: "#000000", // CSS string
+		colorg: "#23ae23", // CSS string
 	};
 
 	var gui = new dat.gui.GUI();
 
-	gui.remember(obj);
+	// gui.remember(obj);
 
-	gui.add(obj, 'message');
-	gui.add(obj, 'displayOutline');
-	gui.add(obj, 'explode');
+	// gui.add(obj, 'message');
+	// gui.add(obj, 'displayOutline');
+	// gui.add(obj, 'explode');
 
-	gui.add(obj, 'maxSize').min(-10).max(10).step(0.25);
-	gui.add(obj, 'height').step(5); // Increment amount
+	// gui.add(obj, 'maxSize').min(-10).max(10).step(0.25);
+	// gui.add(obj, 'height').step(5); // Increment amount
 
-	// Choose from accepted values
-	gui.add(obj, 'type', [ 'one', 'two', 'three' ] );
+	// // Choose from accepted values
+	// gui.add(obj, 'type', [ 'one', 'two', 'three' ] );
 
-	// Choose from named values
-	gui.add(obj, 'speed', { Stopped: 0, Slow: 0.1, Fast: 5 } );
+	// // Choose from named values
+	// gui.add(obj, 'speed', { Stopped: 0, Slow: 0.1, Fast: 5 } );
 	
 	var f1 = gui.addFolder('SpotLight');
 	f1.add(obj, 'posX');
@@ -68,8 +67,11 @@ function main() {
 	f1.add(obj, 'posZ');
 	f1.addColor(obj, 'colorL');
 
-	// var f1 = gui.addFolder('Colors');
-	// f1.addColor(obj, 'color0');
+	var f2 = gui.addFolder('AmbientLight');
+	f2.addColor(obj, 'color0');
+
+	var f3 = gui.addFolder('HemisphereLight');
+	f3.addColor(obj, 'colorg');
 	// f1.addColor(obj, 'color1');
 	// f1.addColor(obj, 'color2');
 	// f1.addColor(obj, 'color3');
@@ -112,54 +114,85 @@ function main() {
 
 	scene.add( spotLight );
 	var spotLightHelper = new THREE.SpotLightHelper( spotLight );
-	scene.add( spotLightHelper );
+	//scene.add( spotLightHelper );
+
+	//Ambient light
+	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	scene.add( light );
+
+	//Hemisphere light
+	var hemisLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+	scene.add( hemisLight );
+
 	//Cube
 	const boxWidth = 5;
 	const boxHeight = 5;
 	const boxDepth = 5;
 	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+	//const geometryTourus = new THREE.TourusGeometry(boxWidth, boxHeight, boxDepth);
 
-	const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+	const material = new THREE.MeshPhongMaterial( { color: 0xaaaaaa } )
 
 	const cube = new THREE.Mesh(geometry, material);
 	cube.castShadow = true; //default is false
 	cube.receiveShadow = false; //default	
 	cube.position.z = 5;
 
-	scene.add(cube);
+	//scene.add(cube);
 	
 	//Create a sphere that cast shadows (but does not receive them)
-	var sphereGeometry = new THREE.SphereBufferGeometry( 1, 12, 12 );
-	var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xaa0000 } );
+	var sphereGeometry = new THREE.SphereBufferGeometry( 5, 12, 12 );
+	var sphereMaterial = new THREE.MeshPhongMaterial( { color: 0xaaaaaa } );
 	var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 	sphere.castShadow = true; //default is false
 	sphere.receiveShadow = false; //default	
 	
 	sphere.position.x = 10;
+	sphere.position.y = 5;
 	sphere.position.z = 5;
 	
 	scene.add( sphere );
-
+	var geometryT = new THREE.TorusKnotBufferGeometry( 5, 3, 100, 16 );
+	var materialT = new THREE.MeshPhongMaterial( { color: 0x79c4ed } );
+	var torusKnot = new THREE.Mesh( geometryT, materialT );
+	torusKnot.castShadow = true; //default is false
+	torusKnot.receiveShadow = false; //default	
+	torusKnot.position.x = -10;
+	torusKnot.position.y = 5;
+	torusKnot.position.z = 5;
+	scene.add( torusKnot );
+	
 	//Create a plane that receives shadows (but does not cast them)
-	var planeGeometry = new THREE.PlaneBufferGeometry( 20, 20, 32, 32 );
-	var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+	var planeGeometry = new THREE.PlaneBufferGeometry( 100, 100, 32, 32 );
+	var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x444444 } )
 	var plane = new THREE.Mesh( planeGeometry, planeMaterial );
 	plane.receiveShadow = true;
-
+	plane.position.y = -5;
+	plane.rotation.x = -0.5 * Math.PI;
 	scene.add( plane );
 	
+	var stats = new Stats();
+	stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+	document.body.appendChild( stats.dom );
+	
 	function render(time) {
-	  	time *= 0.001;  // convert time to seconds
-		spotLight.position.x = obj.posX;
-		spotLight.position.y = obj.posY;
-		spotLight.position.z = obj.posZ;
-		var colorL = new THREE.Color( obj.colorL );
-		spotLight.color.set(obj.colorL);
-		
-		// cube.rotation.x = time;
-		// cube.rotation.y = time;
+		stats.begin();
+			time *= 0.001;  // convert time to seconds
+			spotLight.position.x = obj.posX;
+			spotLight.position.y = obj.posY;
+			spotLight.position.z = obj.posZ;
+			
+			spotLight.color.set(obj.colorL);
+			light.color.set(obj.color0);
+			hemisLight.color.set(obj.color0);
+			hemisLight.groundColor.set(obj.colorg);
+			// cube.rotation.x = time;
+			// cube.rotation.y = time;
 
-		renderer.render(scene, camera);
+			renderer.render(scene, camera);
+		stats.end();
+
+
 		requestAnimationFrame(render);
 	}
 	requestAnimationFrame(render);
