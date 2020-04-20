5bd162b8c9314b79c117d0f70f3f0919adf7d3aa
yuli-ferna <yuliferna123@gmail.com>
2020-04-20 12:09:01 -0400

    load draco and gltf

--- src/index.js
+++ src/index.js
@@ -14,12 +14,16 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 //controles por teclado
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 import { LightShadow } from 'three';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import CameraControls from 'camera-controls';
- 
+
+
 CameraControls.install( { THREE: THREE } );
 const canvas = document.getElementById('canvas');
 const clock = new THREE.Clock();
- 
+ // Optional: Pre-fetch Draco WASM/JS module.
+// dracoLoader.preload();
 //Scene and render
 var renderer, scene, camera, cameraControls;
 var controls;
@@ -46,8 +50,12 @@ let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 
+
+
+
 function init() 
 {
+	
 	//DAT GUI
 	gui = new dat.gui.GUI();
 	obj = {
@@ -75,105 +83,44 @@ function init()
 		helpPoint4:false,
 		
 		intAmbien:1,
-		color0: "#000000", 
+		color0: "#443333", 
 		intHemis:1,
-		colorg: "#23ae23", 
+		colorg: "#111122", 
 	};
 	
 	renderer = new THREE.WebGLRenderer({ canvas });
 	scene = new THREE.Scene();
-	const fov = 45;
+    // scene.fog = new THREE.Fog( 0x443333, 1, 4 );
+
+	const fov = 35;
 	const aspect =  window.innerWidth/ window.innerHeight;  // the canvas default
 	const near = 0.1;
 	const far = 500;
 	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	// controls = new OrbitControls( camera, renderer.domElement );
-	cameraControls = new CameraControls( camera, renderer.domElement );
-	cameraControls.setLookAt( 40, 40, 40, 0.0001, 2, 0, false );
-	cameraControls.maxDistance = 0.0001;
-	cameraControls.minDistance = 0;
-	cameraControls.truckSpeed = 2.0;
-
+	
 	//Lights
-	spotLight = new THREE.SpotLight( 0xffff00 );
-	spotLightHelper = new THREE.SpotLightHelper( spotLight );
+	// spotLight = new THREE.SpotLight( 0xffff00 );
 	light = new THREE.AmbientLight( obj.color0 ); // soft white light
 	hemisLight = new THREE.HemisphereLight( obj.color0, obj.colorg, 1 );
 	
-	//Models in scene
-	
-	//Create a sphere that cast shadows (but does not receive them)
-	var sphereGeometry = new THREE.SphereBufferGeometry( 5, 12, 12 );
-	var sphereMaterial = new THREE.MeshPhongMaterial( { color: 0xaaaaaa } );
-	sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-	
-	//Create a torusKnot that cast shadows (but does not receive them)
-	var geometryT = new THREE.TorusKnotBufferGeometry( 5, 3, 100, 16 );
-	// var materialT = new THREE.MeshPhongMaterial( { color: 0x79c4ed } );
-	torusKnot = new THREE.Mesh( geometryT, sphereMaterial );
-	
-	//Create a plane that receives shadows (but does not cast them)
-	var planeGeometry = new THREE.PlaneBufferGeometry( 100, 100, 32, 32 );
-	var planeMaterial = new THREE.MeshLambertMaterial( { color: 0x444444 } )
-	plane = new THREE.Mesh( planeGeometry, planeMaterial );
 
 	stats = new Stats();
 }
 
 function addLights() 
 {
-	//SpotLight
-	spotLight.castShadow = true;
-	spotLight.position.set( obj.posX, obj.posY, obj.posZ );
-	//Shadow spotlight
-	spotLight.shadow.mapSize.width = 1024;
-	spotLight.shadow.mapSize.height = 1024;
-	spotLight.shadow.camera.near = 1;
-	spotLight.shadow.camera.far = 1000;
-	spotLight.shadow.camera.fov = 30;
-
-	scene.add( spotLight );
-	//Helper
-	scene.add( spotLightHelper );
-	
-	//Ambient light
-	scene.add( light );
 	
 	//Hemisphere light
 	scene.add( hemisLight );
+	spotLight = new THREE.SpotLight();
+    spotLight.angle = Math.PI / 16;
+    spotLight.penumbra = 0.5;
+    spotLight.castShadow = true;
+    spotLight.position.set( - 1, 1, 1 );
+	scene.add( spotLight );
+	spotLightHelper = new THREE.SpotLightHelper( spotLight );
+	scene.add( spotLightHelper );
 	
-	//Point lights
-	var sphereL = new THREE.SphereBufferGeometry( 0.5, 16, 8 );
-	
-	light1 = new THREE.PointLight( 0xff0040, 2, 50 );
-	light1.add( new THREE.Mesh( sphereL, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
-	scene.add( light1 );
-	
-	pLightHelper1 = new THREE.PointLightHelper( light1 );
-	pLightHelper1.visible = false;
-	scene.add( pLightHelper1 );
-	
-	light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
-	light2.add( new THREE.Mesh( sphereL, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
-	scene.add( light2 );
-	pLightHelper2 = new THREE.PointLightHelper( light2 );
-	pLightHelper2.visible = false;
-	scene.add( pLightHelper2 );
-	
-	light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
-	light3.add( new THREE.Mesh( sphereL, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
-	scene.add( light3 );
-	pLightHelper3 = new THREE.PointLightHelper( light3 );
-	pLightHelper3.visible = false;
-	scene.add( pLightHelper3 );
-	
-	light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
-	light4.add( new THREE.Mesh( sphereL, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
-	scene.add( light4 );
-	pLightHelper4 = new THREE.PointLightHelper( light4 );
-	pLightHelper4.visible = false;
-	scene.add( pLightHelper4 );
-
 }
 
 function addGUI() 
@@ -181,30 +128,24 @@ function addGUI()
 	stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 	document.body.appendChild( stats.dom );
 
-	//Spotlight
 	var guiSL = gui.addFolder('SpotLight');
 	guiSL.add(obj, 'helpSpot').onChange(function (val) {
 		spotLightHelper.visible = val;
 	});
 	guiSL.add(obj, 'posX').onChange(function (val) {
 		spotLight.position.x = val;
+		spotLightHelper.update();
 	});
 	guiSL.add(obj, 'posY').onChange(function (val) {
 		spotLight.position.y = val;
+		spotLightHelper.update();
+
 	});
 	guiSL.add(obj, 'posZ').onChange(function (val) {
 		spotLight.position.z = val;
-	});
-	guiSL.addColor(obj, 'colorL').onChange(function (val) {
-		spotLight.color.set(val);
-	});
-	guiSL.add(obj, 'penunmbra').min(0).max(1).step(0.1).onChange(function (val) {
-		spotLight.penunmbra = val;
-	});
-	guiSL.add(obj, 'intSpot').min(0).max(1).step(0.1).onChange(function (val) {
-		spotLight.intensity = val;
-	}).name('Intensity');
+		spotLightHelper.update();
 
+	});
 	//Ambient Light
 	var guiAL = gui.addFolder('AmbientLight');
 	guiAL.addColor(obj, 'color0').onChange(function (val) {
@@ -223,41 +164,6 @@ function addGUI()
 	guiHL.add(obj, 'intHemis').min(0).max(1).step(0.1).onChange(function (val) {
 		hemisLight.intensity = val;
 	}).name('Intensity');
-
-	//Point Lights
-	var guiPL = gui.addFolder('PointLights');
-
-	// Light 1
-	guiPL.add(obj, 'intPoint1').min(0).max(1).step(0.1).onChange(function (val) {
-		light1.intensity = val;
-	}).name('Intensity point 1');
-	guiPL.add(obj, 'helpPoint1').onChange(function (val) {
-		pLightHelper1.visible = val;
-	}).name('Helper light 1');
-
-	// Light 2
-	guiPL.add(obj, 'intPoint2').min(0).max(1).step(0.1).onChange(function (val) {
-		light2.intensity = val;
-	}).name('Intensity point 2');
-	guiPL.add(obj, 'helpPoint2').onChange(function (val) {
-		pLightHelper2.visible = val;
-	}).name('Helper light 2');
-	
-	// Light 3
-	guiPL.add(obj, 'intPoint3').min(0).max(1).step(0.1).onChange(function (val) {
-		light3.intensity = val;
-	}).name('Intensity point 3');
-	guiPL.add(obj, 'helpPoint3').onChange(function (val) {
-		pLightHelper3.visible = val;
-	}).name('Helper light 3');
-	
-	// Light 4
-	guiPL.add(obj, 'intPoint4').min(0).max(1).step(0.1).onChange(function (val) {
-		light4.intensity = val;
-	}).name('Intensity point 4');
-	guiPL.add(obj, 'helpPoint4').onChange(function (val) {
-		pLightHelper4.visible = val;
-	}).name('Helper light 4');
 	
 }
 
@@ -283,44 +189,39 @@ function main() {
 	
 	//Renderer
 	renderer.setClearColor(0x222222);
+    renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 	
 	//Camera
-	camera.position.x = 40;
-	camera.position.y = 40;
-	camera.position.z = 40;
-	camera.lookAt(scene.position);
-
-	//Orbit controls
-	// controls.addEventListener( 'change', render );
-	// controls.minDistance = 20;
-	// controls.maxDistance = 500;
-	// controls.enablePan = false;
+	camera.position.x = 4;
+	camera.position.y = 1;
+	camera.position.z = 5;
+	camera.lookAt( 0, 0.1, 0 );
+	// controls = new OrbitControls( camera, renderer.domElement );
+	cameraControls = new CameraControls( camera, renderer.domElement );
+	// cameraControls.setLookAt( 40, 40, 40, 0.0001, 2, 0, false );
+	cameraControls.maxDistance = 0.0001;
+	cameraControls.minDistance = 0;
+	cameraControls.mouseButtons.wheel =CameraControls.ACTION.NONE;
+	// cameraControls.truckSpeed = 2.0;
 
 	addLights();
+	loadDraco('model/draco/alocasia_s.drc');
+	loadGLTF('model/Duck.gltf');
+	
+    var plane = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry( 8, 8 ),
+        new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+    );
+    plane.rotation.x = - Math.PI / 2;
+    // plane.position.y = 0.03;
+    plane.receiveShadow = true;
+    scene.add( plane );
 
-	//Models in scene
-	sphere.castShadow = true; //default is false
-	sphere.receiveShadow = false; //default	
 	
-	sphere.position.x = 10;
-	sphere.position.y = 5;
-	sphere.position.z = 5;
-	scene.add( sphere );
-	
-	plane.receiveShadow = true;
-	plane.position.y = -5;
-	plane.rotation.x = -0.5 * Math.PI;
-	scene.add( plane );
-	
-	torusKnot.castShadow = true; //default is false
-	torusKnot.receiveShadow = false; //default	
-	torusKnot.position.x = -10;
-	torusKnot.position.y = 5;
-	torusKnot.position.z = 5;
-	scene.add( torusKnot );
 	
 
 	addGUI();
@@ -328,6 +229,77 @@ function main() {
 	// controls.update();
 	
 }
+function loadGLTF(path) {
+	// Instantiate a loader
+	var loader = new GLTFLoader();
+
+	// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+	var dracoLoader = new DRACOLoader();
+	dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
+	dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+	loader.setDRACOLoader( dracoLoader );
+
+	// Load a glTF resource
+	loader.load(
+		// resource URL
+		path,
+		// called when the resource is loaded
+		function ( gltf ) {
+			//Transformations
+			gltf.scene.scale.set(0.5, 0.5, 0.5);
+			gltf.scene.position.set(1, -0.05, 0);
+			gltf.scene.castShadow = true;
+			gltf.scene.receiveShadow = true;
+
+			scene.add( gltf.scene );
+			console.log(gltf);
+			// gltf.scale.set(2,2,2);
+			gltf.animations; // Array<THREE.AnimationClip>
+			gltf.scene; // THREE.Group
+			gltf.scenes; // Array<THREE.Group>
+			gltf.cameras; // Array<THREE.Camera>
+			gltf.asset; // Object
+
+		},
+		// called while loading is progressing
+		function ( xhr ) {
+
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+		},
+		// called when loading has errors
+		function ( error ) {
+
+			console.log( 'An error happened' );
+
+		});	
+}
+function loadDraco(path) {
+	var dracoLoader = new DRACOLoader();
+	// It is recommended to always pull your Draco JavaScript and WASM decoders
+	// from this URL. Users will benefit from having the Draco decoder in cache
+	// as more sites start using the static URL.
+	dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+	
+	dracoLoader.setDecoderConfig( { type: 'js' } );
+
+	dracoLoader.load( path, function ( geometry ) {
+
+		geometry.computeVertexNormals();
+
+		var material = new THREE.MeshStandardMaterial( { color: 0x606060 } );
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.castShadow = true;
+		mesh.receiveShadow = true;
+		// mesh.position.y = 0.3;
+		scene.add( mesh );
+
+		// Release decoder resources.
+		dracoLoader.dispose();
+
+	} );
+}
+
 //Event function when a key is pressed
 let onKeyDown = function ( event ) {
 	switch ( event.keyCode ) {
@@ -351,17 +323,7 @@ let onKeyDown = function ( event ) {
 			moveRight = true;
 			movement(moveRight, speedMovement);
 			break;
-		case 70: //F
-			audioManager.startAudio(redSphere);
-			break;
-		case 67:
-		if(mediaRecorder == null) {
-			catchMicrophone(blueSphere);
-		}else {
-			blueSphere.getObjectByName('audio').setVolume(10);
-		} 
-
-			break;
+		
 		}
 
 };
@@ -384,17 +346,8 @@ let onKeyUp = function ( event ) {
 		case 68: // d
 			moveRight = false;
 			break;
-		case 67:
-			blueSphere.getObjectByName('audio').setVolume(0);
-			break;
 		}
 };
-//event function on click
-//this create all the sounds elements
-const clicker = function(event){
-	principal.style.display = 'none';
-	canvas.style.display = 'block';
-}
 
 
 function displayWindowSize(){
@@ -423,32 +376,17 @@ function animate()
 	render();
 	// controls.update();
 	stats.update();	
-	// if ( hasControlsUpdated ) {
+	if ( hasControlsUpdated ) {
  
-    //     renderer.render( scene, camera );
+		// renderer.render( scene, camera );
  
-    // }
+    }
 	renderer.render(scene, camera);
 }
 
 function render() 
 {
-	var time = Date.now() * 0.001;  
-	light1.position.x = Math.sin( time * 0.7 ) * 30;
-	light1.position.y = Math.cos( time * 0.5 ) * 40;
-	light1.position.z = Math.cos( time * 0.3 ) * 30;
-
-	light2.position.x = Math.cos( time * 0.3 ) * 30;
-	light2.position.y = Math.sin( time * 0.5 ) * 40;
-	light2.position.z = Math.sin( time * 0.7 ) * 30;
-
-	light3.position.x = Math.sin( time * 0.7 ) * 30;
-	light3.position.y = Math.cos( time * 0.3 ) * 40;
-	light3.position.z = Math.sin( time * 0.5 ) * 30;
-
-	light4.position.x = Math.sin( time * 0.3 ) * 30;
-	light4.position.y = Math.cos( time * 0.7 ) * 40;
-	light4.position.z = Math.sin( time * 0.5 ) * 30;
+	
 	
 }
 
